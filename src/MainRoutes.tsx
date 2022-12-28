a11ed57001fb1028873de621e3a480b39a5d891c
Thiago <thiagofang@gmail.com>
2022-12-28 16:30:56 -0300

    home: creating first files from home page

--- src/MainRoutes.tsx
+++ src/MainRoutes.tsx
@@ -1,8 +1,9 @@
 import { useRoutes } from "react-router-dom";
+import { Home } from "./pages/home";
 
 export const MainRoutes = () => {
   return useRoutes ([
-    {path: "/", element: <div>Home</div>},
+    {path: "/", element: <Home />},
     {path: "/about", element: <div>About</div>},
     {path: "/projects", element: <div>projetos</div>},
   ]);
