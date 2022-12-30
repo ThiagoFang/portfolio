a31cf3ba8eb6ddcc63ead4f5e611ab44e5f211f3
Thiago <thiagofang@gmail.com>
2022-12-30 00:09:51 -0300

    banner: component to play and pause the background video

--- src/components/Home/Banner/Banner.tsx
+++ src/components/Home/Banner/Banner.tsx
@@ -1,14 +1,41 @@
-import { BannerTitle } from '../BannerTitle';
-import * as C from './styles';
-import videoBg from '/assets/video/video_color.mp4'
+import { useRef, useState } from "react";
+import { BannerTitle } from "../BannerTitle";
+import * as C from "./styles";
+import videoBg from "/assets/video/video_color.mp4";
 
 export const Banner = () => {
+  const [pause, setPause] = useState(true);
+
+  const videoRef: React.MutableRefObject<HTMLVideoElement | null> =
+    useRef(null);
+
+  const handleToggleVideo = () => {
+    const video = videoRef.current;
+    if (video) {
+      if (video.paused) {
+        video.play();
+        setPause(true);
+        return;
+      }
+      video.pause();
+      setPause(false);
+    }
+  };
+
   return (
     <C.Container>
-      <C.Video src={videoBg} autoPlay loop muted />
+      <C.Video ref={videoRef} src={videoBg} autoPlay loop muted />
       <C.ContentArea>
+        <C.PlayPause
+          onClick={handleToggleVideo}
+          className={`ri-pause-fill ${pause ? "active" : ""}`}
+        ></C.PlayPause>
+        <C.PlayPause
+          onClick={handleToggleVideo}
+          className={`ri-play-mini-line ${pause ? "" : "active"}`}
+        ></C.PlayPause>
         <BannerTitle />
       </C.ContentArea>
     </C.Container>
-  )
-}
+  );
+};
--- src/components/Home/Banner/styles.ts
+++ src/components/Home/Banner/styles.ts
@@ -11,7 +11,7 @@ export const Video = styled.video`
   width: 100%;
   height: 100%;
   object-fit: cover;
-  opacity: .2;
+  opacity: 0.2;
 `;
 
 export const ContentArea = styled.div`
@@ -24,5 +24,23 @@ export const ContentArea = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  color: #FFF;
+  color: #fff;
+`;
+
+export const PlayPause = styled.i`
+  position: absolute;
+  background-color: var(--secondary-background);
+  border: 0.5px solid var(--main-color);
+  border-radius: 0.4rem;
+  padding: 0.8rem 1.6rem;
+  bottom: 3.2rem;
+  right: 3.2rem;
+  font-size: 3.2;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+  display: none;
+
+  &.active {
+    display: flex;
+  }
 `;
