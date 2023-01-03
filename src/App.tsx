f9131a71a4f3e738392c7dd1d6eef3a4f22606d0
Thiago <thiagofang@gmail.com>
2023-01-03 16:39:54 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { ScrollUpArrow } from "./components/common/ScrollUpArrow";
 import { MainRoutes } from "./MainRoutes";
-import { AnimatedRoutes } from "./animatedRoutes";
-
 const App = () => {
   return (
     <>
       <Header />
       <ScrollUpArrow />
-      <AnimatedRoutes />
+      <MainRoutes />
       <Footer />
     </>
   );
