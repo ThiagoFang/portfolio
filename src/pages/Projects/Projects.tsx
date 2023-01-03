22832070734d83d71c74bbb3cbf48edf3bdf8bc0
Thiago <thiagofang@gmail.com>
2023-01-03 16:34:26 -0300

    changes projects background

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -5,20 +5,22 @@ import * as C from "./styles";
 export const Projects = () => {
   return (
     <C.Container>
-      <C.Content>
-        <SectionTitle
-          normalFont="My"
-          grossFont="Projects"
-          id="projects"
-          subtitle="Meus Projetos"
-        />
-        <C.ProjectsGrid>
-          <ProjectBox />
-          <ProjectBox />
-          <ProjectBox />
-          <ProjectBox />
-        </C.ProjectsGrid>
-      </C.Content>
+      <C.Filter>
+        <C.Content>
+          <SectionTitle
+            normalFont="My"
+            grossFont="Projects"
+            id="projects"
+            subtitle="Meus Projetos"
+          />
+          <C.ProjectsGrid>
+            <ProjectBox />
+            <ProjectBox />
+            <ProjectBox />
+            <ProjectBox />
+          </C.ProjectsGrid>
+        </C.Content>
+      </C.Filter>
     </C.Container>
   );
 };
--- src/pages/Projects/styles.ts
+++ src/pages/Projects/styles.ts
@@ -1,20 +1,67 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
 
 export const Container = styled.section`
+  display: flex;
+  flex-direction: column;
   position: relative;
-  min-height: 100vh;
-  padding: 2rem;
-  padding-top: 12rem;
+  background-color: var(--secondary-background);
+  z-index: -2;
 
-  #projects {
-    margin: 0;
+  ::before,
+  ::after {
+    content: " ";
+    position: absolute;
+    z-index: 0;
+    display: flex;
+    opacity: 0.1;
+    background-color: var(--main-color);
+    border-radius: 50%;
   }
+
+  ::before {
+    width: 50rem;
+    height: 50rem;
+    bottom: 6rem;
+    left: -30rem;
+    animation: beforeBounce 8s linear infinite;
+  }
+
+  ::after {
+    width: 35rem;
+    height: 35rem;
+    top: 6rem;
+    z-index: -1;
+    right: -25rem;
+    animation: afterBounce 8s linear infinite;
+    animation-delay: 4s;
+  }
+
+  @keyframes beforeBounce {
+    60% {
+      transform: scale(1.3) translate(50px);
+    }
+  }
+
+  @keyframes afterBounce {
+    60% {
+      transform: scale(1.3) translate(-50px);
+    }
+  }
+`;
+
+export const Filter = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  padding-top: 12rem;
+  height: 100%;
+  backdrop-filter: blur(10rem);
 `;
 
 export const Content = styled.div`
   display: flex;
   flex-direction: column;
+  width: 100%;
+  height: 100%;
   align-items: center;
   gap: 3.2rem;
 `;
