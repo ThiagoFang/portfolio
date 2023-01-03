9ee1933e84e0f64db1e1b66257a025111bf35232
Thiago <thiagofang@gmail.com>
2023-01-03 11:15:18 -0300

    Pages: files setup for projects page

--- src/MainRoutes.tsx
+++ src/MainRoutes.tsx
@@ -1,11 +1,12 @@
 import { useRoutes } from "react-router-dom";
 import { Home } from "./pages/home";
+import { Projects } from "./pages/Projects";
 
 export const MainRoutes = () => {
   return useRoutes([
     { path: "/", element: <Home /> },
     { path: "/about", element: <div>About</div> },
-    { path: "/projects", element: <div>projetos</div> },
+    { path: "/projects", element: <Projects /> },
     { path: "/art", element: <div>art</div> },
     { path: "/contact", element: <div>contact</div> },
   ]);
---
+++ src/pages/Projects/Projects.tsx
@@ -0,0 +1,11 @@
+import * as C from "./styles";
+
+import React from "react";
+
+export const Projects = () => {
+  return (
+    <C.Container>
+      <div>Projects</div>
+    </C.Container>
+  );
+};
