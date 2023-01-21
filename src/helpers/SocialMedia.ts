a5bdd9cee0580d7476a664d29769e0e60ed5867d
Thiago <thiagofang@gmail.com>
2023-01-21 14:46:27 -0300

    file with all my social media infos

---
+++ src/helpers/SocialMedia.ts
@@ -0,0 +1,30 @@
+export type ISocialMedias = ISocialMedia[];
+
+export interface ISocialMedia {
+  name: string;
+  icon: string;
+  link: string;
+}
+
+export const SocialMedias: ISocialMedias = [
+  {
+    name: "twitter",
+    icon: "ri-twitter-fill",
+    link: "https://twitter.com/Thiago_Fang",
+  },
+  {
+    name: "behance",
+    icon: "ri-behance-fill",
+    link: "https://www.behance.net/Thiago-s-lima",
+  },
+  {
+    name: "instagram",
+    icon: "ri-instagram-fill",
+    link: "https://www.instagram.com/thiago_fang/",
+  },
+  {
+    name: "linkedin",
+    icon: "ri-linkedin-box-fill",
+    link: "https://www.linkedin.com/in/thiago-lima-46463715a/",
+  },
+];
