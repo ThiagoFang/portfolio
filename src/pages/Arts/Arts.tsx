b985371a274c802c49ae24aaffa49dbacac3bb91
Thiago <thiagofang@gmail.com>
2023-01-24 10:25:31 -0300

    configuration to initial load not load again after an reload on art page

--- src/pages/Arts/Arts.tsx
+++ src/pages/Arts/Arts.tsx
@@ -1,7 +1,15 @@
 import { SectionTitle } from "../../components/common/SectionTitle";
 import * as C from "./styles";
+import { ConfigActions, useConfig } from "../../context/ConfigContext";
+import { useEffect } from "react";
 
 export const Arts = () => {
+  const { state, dispatch } = useConfig();
+
+  useEffect(() => {
+    dispatch({ type: ConfigActions.setInitialLoading, payload: false });
+  }, []);
+
   return (
     <C.Container>
       <SectionTitle
