c9eabb1f370c530480098f8f6f7984e3ba3b78ff
Thiago <thiagofang@gmail.com>
2023-01-27 14:26:06 -0300

    responsibity in contact page

--- src/pages/Contact/styles.ts
+++ src/pages/Contact/styles.ts
@@ -3,20 +3,33 @@ import styled from "styled-components";
 export const Container = styled.div`
   background-color: var(--secondary-background);
   min-height: 100vh;
-  padding-top: 12rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  #contactTitle {
+    margin: 0;
+    margin-bottom: 6.4rem;
+  }
 `;
 
 export const Form = styled.form`
   display: grid;
   max-width: 768px;
   grid-template-columns: repeat(2, 1fr);
-  margin: auto;
   column-gap: 6.4rem;
   row-gap: 3.2rem;
 
   #contactTextArea,
   #buttonsArea {
     grid-column: 1 / 3;
+  }
+
+  @media screen and (max-width: 425px) {
+    display: flex;
+    flex-direction: column;
+    padding: 0 2.4rem;
   }
 `;
 
