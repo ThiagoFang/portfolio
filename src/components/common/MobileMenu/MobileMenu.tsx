cffe877fa9f2a4946914530e40f81677bed3782a
Thiago <thiagofang@gmail.com>
2023-01-27 15:59:50 -0300

    adding mobile menu

--- src/components/common/MobileMenu/MobileMenu.tsx
+++ src/components/common/MobileMenu/MobileMenu.tsx
@@ -1,3 +1,4 @@
+import { useLocation, useNavigate } from "react-router-dom";
 import * as C from "./styles";
 
 interface IProps {
@@ -6,12 +7,52 @@ interface IProps {
 }
 
 export const MobileMenu = ({ menu, changeAction }: IProps) => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const handleNavigate = (path: string) => {
+    navigate(path);
+    changeAction(false);
+  };
+
   return (
     <C.Container className={menu ? "open" : ""}>
       <C.CloseIcon
         onClick={() => changeAction(false)}
         className="ri-close-fill"
       />
+      <C.NavArea>
+        <C.NavItem
+          onClick={() => handleNavigate("/")}
+          className={location.pathname === "/" ? "selected" : ""}
+        >
+          HOME
+        </C.NavItem>
+        <C.NavItem
+          onClick={() => handleNavigate("/about")}
+          className={location.pathname === "/about" ? "selected" : ""}
+        >
+          SOBRE MIM
+        </C.NavItem>
+        <C.NavItem
+          onClick={() => handleNavigate("/projects")}
+          className={location.pathname === "/projects" ? "selected" : ""}
+        >
+          PROJETOS
+        </C.NavItem>
+        <C.NavItem
+          onClick={() => handleNavigate("/art")}
+          className={location.pathname === "/art" ? "selected" : ""}
+        >
+          ART
+        </C.NavItem>
+        <C.NavItem
+          onClick={() => handleNavigate("/contact")}
+          className={location.pathname === "/contact" ? "selected" : ""}
+        >
+          CONTATO
+        </C.NavItem>
+      </C.NavArea>
     </C.Container>
   );
 };
--- src/components/common/MobileMenu/styles.ts
+++ src/components/common/MobileMenu/styles.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.aside`
   position: fixed;
+  padding: 3.2rem 1.6rem;
   top: 0;
   left: 0;
   width: 100vw;
@@ -23,4 +24,23 @@ export const CloseIcon = styled.i`
   color: var(--main-color);
   font-size: var(--h1-font-size);
   cursor: pointer;
+`;
+
+export const NavArea = styled.ul`
+  display: flex;
+  flex-direction: column;
+  margin-top: 3.2rem;
+  gap: 2.4rem;
+  align-items: center;
+`;
+
+export const NavItem = styled.li`
+  color: var(--text-color);
+  font-size: var(--h2-font-size);
+  color: var(--text-light);
+  font-weight: 600;
+
+  &.selected {
+    color: var(--text-color);
+  }
 `;
