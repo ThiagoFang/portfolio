7811f77b1f0af606920024b929ffad68d584949e
Thiago <thiagofang@gmail.com>
2023-01-30 15:39:37 -0300

    basic file setup for settings modal

---
+++ src/components/common/Configurations/Configurations.tsx
@@ -0,0 +1,11 @@
+import * as C from "./styles";
+
+export const Configurations = () => {
+  return (
+    <C.BlackArea>
+      <C.Modal>
+        <div>Configurations</div>
+      </C.Modal>
+    </C.BlackArea>
+  );
+};
---
+++ src/components/common/Configurations/styles.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const BlackArea = styled.div``;
+
+export const Modal = styled.div``;
