2c12e728ec508421f07c6ec5b7ea091eee1755ab
Thiago <thiagofang@gmail.com>
2023-01-31 17:00:52 -0300

    translating all the home page

--- src/components/Home/AboutMe/AboutMe.tsx
+++ src/components/Home/AboutMe/AboutMe.tsx
@@ -2,8 +2,33 @@ import { SectionTitle } from "../../common/SectionTitle";
 import * as C from "./styles";
 import { FilledButton } from "../../common/Buttons/FilledButton";
 import { NoBGButton } from "../../common/Buttons/NoBGButton";
+import { useConfig } from "../../../context/ConfigContext";
+
+const PortugueseDesc = () => {
+  return (
+    <C.Desc>
+      Olá! Meu nome é <span>Thiago Silva</span>, também conhecido como
+      <span> Fang</span>, sou apaixonado por técnologia desde pequeno, meu
+      primeiro contato com programação foi no ensino fundamental onde tentei
+      criar a minha própria versão de um jogo que eu jogava muito.
+    </C.Desc>
+  );
+};
+
+export const EnglishDesc = () => {
+  return (
+    <C.Desc>
+      Hello! My name is <span>Thiago Silva</span>, also known as
+      <span> Fang</span>, I have been passionate about technology since I was
+      young, my first contact with programming was in elementary school where I
+      tried to create my own version of a game I used to play a lot.
+    </C.Desc>
+  );
+};
 
 export const AboutMe = () => {
+  const configs = useConfig().state;
+
   return (
     <C.Container>
       <SectionTitle
@@ -15,24 +40,23 @@ export const AboutMe = () => {
       <C.InfoContainer>
         <C.MainInfo>
           <C.Info>
-            <C.Title>Quem eu sou?</C.Title>
-            <C.Desc>
-              Olá! Meu nome é <span>Thiago Silva</span>, também conhecido como
-              <span> Fang</span>, sou apaixonado por técnologia desde pequeno,
-              meu primeiro contato com programação foi no ensino fundamental
-              onde tentei criar a minha própria versão de um jogo que eu jogava
-              muito.
-            </C.Desc>
+            <C.Title>
+              {configs.language === "pt" ? "Quem eu sou?" : "Who am i?"}
+            </C.Title>
+            {configs.language === "pt" && <PortugueseDesc />}
+            {configs.language === "en" && <EnglishDesc />}
             <C.ButtonsArea>
               <FilledButton
                 icon="ri-arrow-drop-right-line"
-                title="Meus Projetos"
+                title={
+                  configs.language === "pt" ? "Meus Projetos" : "My Projects"
+                }
                 isLink={false}
                 to="/projects"
               />
               <NoBGButton
                 icon="ri-arrow-right-up-line"
-                title="Veja Mais"
+                title={configs.language === "pt" ? "Veja Mais" : "See More"}
                 isLink={false}
                 to="/about"
               />
@@ -42,17 +66,23 @@ export const AboutMe = () => {
           <C.Numbers>
             <C.NumberBox className="number">
               <C.NumberTitle>5+</C.NumberTitle>
-              <C.NumberDesc>Tecnologias</C.NumberDesc>
+              <C.NumberDesc>
+                {configs.language === "pt" ? "Tecnologias" : "Technologies"}
+              </C.NumberDesc>
             </C.NumberBox>
 
             <C.NumberBox className="number">
               <C.NumberTitle>20+</C.NumberTitle>
-              <C.NumberDesc>Projetos</C.NumberDesc>
+              <C.NumberDesc>
+                {configs.language === "pt" ? "Projetos" : "Projects"}
+              </C.NumberDesc>
             </C.NumberBox>
 
             <C.NumberBox className="number">
               <C.NumberTitle>1+</C.NumberTitle>
-              <C.NumberDesc>Anos como dev</C.NumberDesc>
+              <C.NumberDesc>
+                {configs.language === "pt" ? "Anos como dev" : "Years as Dev"}
+              </C.NumberDesc>
             </C.NumberBox>
           </C.Numbers>
         </C.MainInfo>
