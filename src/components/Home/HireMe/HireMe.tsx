53621eb0e07de43c983685c89b2f51a8a96a12d0
Thiago <thiagofang@gmail.com>
2023-01-31 16:52:19 -0300

    translating Hire Me page

--- src/components/Home/HireMe/HireMe.tsx
+++ src/components/Home/HireMe/HireMe.tsx
@@ -1,20 +1,30 @@
+import { useConfig } from "../../../context/ConfigContext";
 import { FilledButton } from "../../common/Buttons/FilledButton";
 import { NoBGButton } from "../../common/Buttons/NoBGButton";
 import * as C from "./styles";
 
 export const HireMe = () => {
+  const configs = useConfig().state;
+
   return (
     <C.Container>
-      <C.Title>Precisando de alguem para desenvolver sua aplicação?</C.Title>
+      <C.Title>
+        {configs.language === "pt" && (
+          <>Precisando de alguem para desenvolver sua aplicação?</>
+        )}
+        {configs.language === "en" && (
+          <>Need someone to develop your application?</>
+        )}
+      </C.Title>
       <C.ButtonsArea>
         <FilledButton
-          title="Meus Projetos"
+          title={configs.language === "pt" ? "Meus Projetos" : "My Projects"}
           isLink={false}
           to="/projects"
           icon="ri-arrow-drop-right-line"
         />
         <NoBGButton
-          title="Contato"
+          title={configs.language === "pt" ? "Contato" : "Contact"}
           to="/about"
           icon="ri-arrow-right-up-line"
           isLink={false}
